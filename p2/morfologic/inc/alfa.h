f7f9966093ac9dae71a0eee5c2f7d9743e61c7a8
alvarorp00 <alvaro.rodriguezp@estudiante.uam.es>
2020-11-18 17:18:13 +0100

    strncpy is now COPY

--- p2/morfologic/inc/alfa.h
+++ p2/morfologic/inc/alfa.h
@@ -1,12 +1,15 @@
 #ifndef ALFA_H
 #define ALFA_H
 
-#define STRINGIFY(x) #x
+#define STR(x) #x
 
 #define READ(file) fopen(file, "r");
 #define WRITE(file) fopen(file, "w");
 
 #define BUFF 128
+
+#define COPY(te, tf) \
+            strncpy(te, tf, BUFF)
 
 extern char errbuff[BUFF];
 
